210923e1b77b3a4764a7919dfb8fd6a0c747458e
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-03-11 11:12:48 +0400

    test request

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,11 +1,18 @@
 const graphql = require('graphql')
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql
+const {GraphQLObjectType, GraphQLString, GraphQLSchema , GraphQLID} = graphql
+
+const movies = [
+    {id: '1', name: 'Film 1', genre: 'Criminal'},
+    {id: '2', name: 'Film 2 ', genre: 'Comedy'},
+    {id: 3, name: 'Film 3', genre: 'Drama'},
+    {id: 4, name: 'Film 4', genre: 'Fiz'},
+]
 
 const MovieType = new GraphQLObjectType({
     name: 'Movie',
     fields: () => ({
-        id: {type: GraphQLString},
+        id: {type: GraphQLID },
         name: {type: GraphQLString},
         genre: {type: GraphQLString},
     })
@@ -16,9 +23,9 @@ const Query = new GraphQLObjectType({
     fields: {
         movie: {
             type: MovieType,
-            args: {id: {type: GraphQLString}},
+            args: {id: {type: GraphQLID}},
             resolve(parent, args) {
-
+                return movies.find(movie => movie.id == args.id)
             }
         }
     }
